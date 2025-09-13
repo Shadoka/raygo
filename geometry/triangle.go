b399b0a023d7d19803bd34d67c6aa6ce3401faba
Shadoka <tilmann.groeger@gmx.de>
2025-09-13 19:29:39 +0200

    added triangle shape

---
+++ geometry/triangle.go
@@ -0,0 +1,124 @@
+package geometry
+
+import (
+	gomath "math"
+	"raygo/math"
+	"reflect"
+
+	"github.com/google/uuid"
+)
+
+type Triangle struct {
+	P1        math.Point
+	P2        math.Point
+	P3        math.Point
+	E1        math.Vector // Direction P1 -> P2
+	E2        math.Vector // Direction P1 -> P3
+	Normal    math.Vector
+	Id        string
+	Transform math.Matrix
+	Material  Material
+	Parent    *Group
+}
+
+func CreateTriangle(p1 math.Point, p2 math.Point, p3 math.Point) *Triangle {
+	e1 := p2.Subtract(p1)
+	e2 := p3.Subtract(p1)
+	return &Triangle{
+		P1:        p1,
+		P2:        p2,
+		P3:        p3,
+		E1:        e1,
+		E2:        e2,
+		Normal:    e1.Cross(e2).Normalize(),
+		Id:        uuid.NewString(),
+		Transform: math.IdentityMatrix(),
+		Material:  DefaultMaterial(),
+		Parent:    nil,
+	}
+}
+
+func (t *Triangle) Equals(other Shape) bool {
+	result := reflect.TypeOf(t) == reflect.TypeOf(other) &&
+		t.Transform.Equals(other.GetTransform()) &&
+		t.Material.Equals(*other.GetMaterial()) &&
+		t.Parent == other.GetParent()
+
+	if result {
+		otherTriangle := other.(*Triangle)
+		result = result &&
+			t.P1.Equals(otherTriangle.P1) &&
+			t.P2.Equals(otherTriangle.P2) &&
+			t.P3.Equals(otherTriangle.P3)
+	}
+
+	return result
+}
+
+func (t *Triangle) GetId() string {
+	return t.Id
+}
+
+func (t *Triangle) SetTransform(m math.Matrix) {
+	t.Transform = m
+}
+
+func (t *Triangle) GetTransform() math.Matrix {
+	return t.Transform
+}
+
+func (t *Triangle) SetMaterial(m Material) {
+	t.Material = m
+}
+
+func (t *Triangle) GetMaterial() *Material {
+	return &t.Material
+}
+
+func (t *Triangle) NormalAt(p math.Point) math.Vector {
+	return t.Normal
+}
+
+func (t *Triangle) GetParent() *Group {
+	return t.Parent
+}
+
+func (t *Triangle) SetParent(g *Group) {
+	t.Parent = g
+}
+
+func (t *Triangle) Bounds() *Bounds {
+	return &Bounds{}
+}
+
+func (t *Triangle) Intersect(ray Ray) []Intersection {
+	return make([]Intersection, 0)
+}
+
+func (t *Triangle) localIntersect(localRay Ray) []Intersection {
+	dirCrossE2 := localRay.Direction.Cross(t.E2)
+	determinant := t.E1.Dot(dirCrossE2)
+
+	xs := make([]Intersection, 0)
+	if gomath.Abs(determinant) < math.EPSILON {
+		return xs
+	}
+
+	f := 1.0 / determinant
+	p1ToOrigin := localRay.Origin.Subtract(t.P1)
+	u := f * p1ToOrigin.Dot(dirCrossE2)
+
+	if u < 0 || u > 1 {
+		return xs
+	}
+
+	originCrossE1 := p1ToOrigin.Cross(t.E1)
+	v := f * localRay.Direction.Dot(originCrossE1)
+
+	if v < 0 || (u+v) > 1 {
+		return xs
+	}
+
+	t2 := f * t.E2.Dot(originCrossE1)
+	return append(xs, CreateIntersection(t2, t))
+}
---
+++ geometry/triangle_test.go
@@ -0,0 +1,87 @@
+package geometry
+
+import (
+	"raygo/math"
+	"testing"
+
+	"gotest.tools/v3/assert"
+)
+
+var p = math.CreatePoint
+var v = math.CreateVector
+
+func TestCreateTriangle(t *testing.T) {
+	p1 := math.CreatePoint(0.0, 1.0, 0.0)
+	p2 := math.CreatePoint(-1.0, 0.0, 0.0)
+	p3 := math.CreatePoint(1.0, 0.0, 0.0)
+	e1 := math.CreateVector(-1.0, -1.0, 0.0)
+	e2 := math.CreateVector(1.0, -1.0, 0.0)
+	normal := math.CreateVector(0.0, 0.0, 1.0)
+
+	tri := CreateTriangle(p1, p2, p3)
+
+	assert.Assert(t, p1.Equals(tri.P1))
+	assert.Assert(t, p2.Equals(tri.P2))
+	assert.Assert(t, p3.Equals(tri.P3))
+	assert.Assert(t, e1.Equals(tri.E1))
+	assert.Assert(t, e2.Equals(tri.E2))
+	assert.Assert(t, normal.Equals(tri.Normal))
+}
+
+func TestNormalAt(t *testing.T) {
+	p1 := p(0.0, 0.5, 0.0)
+	p2 := p(-0.5, 0.75, 0.0)
+	p3 := p(0.5, 0.25, 0.0)
+
+	tri := CreateTriangle(p(0.0, 1.0, 0.0), p(-1.0, 0.0, 0.0), p(1.0, 0.0, 0.0))
+
+	assert.Assert(t, tri.Normal.Equals(tri.NormalAt(p1)))
+	assert.Assert(t, tri.Normal.Equals(tri.NormalAt(p2)))
+	assert.Assert(t, tri.Normal.Equals(tri.NormalAt(p3)))
+}
+
+func TestLocalIntersectParallelRay(t *testing.T) {
+	tri := CreateTriangle(p(0.0, 1.0, 0.0), p(-1.0, 0.0, 0.0), p(1.0, 0.0, 0.0))
+	r := CreateRay(p(0.0, -1.0, -2.0), v(0.0, 1.0, 0.0))
+
+	xs := tri.localIntersect(r)
+
+	assert.Assert(t, len(xs) == 0)
+}
+
+func TestLocalIntersectP1P3(t *testing.T) {
+	tri := CreateTriangle(p(0.0, 1.0, 0.0), p(-1.0, 0.0, 0.0), p(1.0, 0.0, 0.0))
+	r := CreateRay(p(1.0, 1.0, -2.0), v(0.0, 0.0, 1.0))
+
+	xs := tri.localIntersect(r)
+
+	assert.Assert(t, len(xs) == 0)
+}
+
+func TestLocalIntersectP1P2(t *testing.T) {
+	tri := CreateTriangle(p(0.0, 1.0, 0.0), p(-1.0, 0.0, 0.0), p(1.0, 0.0, 0.0))
+	r := CreateRay(p(-1.0, 1.0, -2.0), v(0.0, 0.0, 1.0))
+
+	xs := tri.localIntersect(r)
+
+	assert.Assert(t, len(xs) == 0)
+}
+
+func TestLocalIntersectP2P3(t *testing.T) {
+	tri := CreateTriangle(p(0.0, 1.0, 0.0), p(-1.0, 0.0, 0.0), p(1.0, 0.0, 0.0))
+	r := CreateRay(p(0.0, -1.0, -2.0), v(0.0, 0.0, 1.0))
+
+	xs := tri.localIntersect(r)
+
+	assert.Assert(t, len(xs) == 0)
+}
+
+func TestLocalIntersectHit(t *testing.T) {
+	tri := CreateTriangle(p(0.0, 1.0, 0.0), p(-1.0, 0.0, 0.0), p(1.0, 0.0, 0.0))
+	r := CreateRay(p(0.0, 0.5, -2.0), v(0.0, 0.0, 1.0))
+
+	xs := tri.localIntersect(r)
+
+	assert.Assert(t, len(xs) == 1)
+	assert.Assert(t, floatEquals(xs[0].IntersectionAt, 2.0))
+}
