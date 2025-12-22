19122bc7e009796432c5826d2840897f2de1747a
Shadoka <tilmann.groeger@gmx.de>
2025-12-22 15:14:20 +0100

    added some more yaml file structs

--- parser/file_structs.go
+++ parser/file_structs.go
@@ -1,7 +1,10 @@
 package parser
 
 type YamlDescription struct {
-	Colors []ColorModel `name:"colors"`
+	Colors    []ColorModel         `yaml:"colors"`
+	Materials []NamedMaterialModel `yaml:"materials"`
+	Patterns  PatternContainer     `yaml:"patterns"`
+	Scene     SceneContainer       `yaml:"scene"`
 }
 
 type ColorModel struct {
@@ -9,4 +12,88 @@ type ColorModel struct {
 	R    int    `yaml:"r"`
 	G    int    `yaml:"g"`
 	B    int    `yaml:"b"`
+}
+
+type PatternContainer struct {
+	Checker  []CheckerPatternModel  `yaml:"checker"`
+	Ring     []RingPatternModel     `yaml:"ring"`
+	Gradient []GradientPatternModel `yaml:"gradient"`
+	Stripe   []StripePatternModel   `yaml:"stripe"`
+}
+
+// embedding struct
+type DualColorPattern struct {
+	Name       string            `yaml:"name"`
+	ColorA     string            `yaml:"colorA"`
+	ColorB     string            `yaml:"colorB"`
+	Transforms []*TransformModel `yaml:"transforms"`
+}
+
+type CheckerPatternModel struct {
+	DualColorPattern `yaml:",inline"`
+}
+
+type GradientPatternModel struct {
+	DualColorPattern `yaml:",inline"`
+}
+
+type RingPatternModel struct {
+	DualColorPattern `yaml:",inline"`
+}
+
+type StripePatternModel struct {
+	DualColorPattern `yaml:",inline"`
+}
+
+type TransformModel struct {
+	Type string  `yaml:"type"`
+	X    float64 `yaml:"x"`
+	Y    float64 `yaml:"y"`
+	Z    float64 `yaml:"z"`
+}
+
+type NamedTransformModel struct {
+	TransformModel `yaml:",inline"`
+	Name           string `yaml:"name"`
+}
+
+type MaterialModel struct {
+	Color           string  `yaml:"color"`
+	Pattern         string  `yaml:"pattern"`
+	Ambient         float64 `yaml:"ambient"`
+	Diffuse         float64 `yaml:"diffuse"`
+	Specular        float64 `yaml:"specular"`
+	Shininess       float64 `yaml:"shininess"`
+	Reflective      float64 `yaml:"reflective"`
+	Transparency    float64 `yaml:"transparency"`
+	RefractiveIndex float64 `yaml:"refractiveIndex"`
+}
+
+type NamedMaterialModel struct {
+	MaterialModel `yaml:",inline"`
+	Name          string `yaml:"name"`
+}
+
+type SceneContainer struct {
+	Planes []*PlaneModel `yaml:"planes"`
+}
+
+type CommonSceneObject struct {
+	Name       string            `yaml:"name"`
+	Parent     string            `yaml:"parent"`
+	Material   string            `yaml:"material"`
+	Transform  string            `yaml:"transform"`
+	Transforms []*TransformModel `yaml:"transforms"`
+}
+
+type PlaneModel struct {
+	CommonSceneObject `yaml:",inline"`
+}
+
+type SphereModel struct {
+	CommonSceneObject `yaml:",inline"`
+}
+
+type CubeModel struct {
+	CommonSceneObject `yaml:",inline"`
 }
--- parser/yaml_parser_test.go
+++ parser/yaml_parser_test.go
@@ -31,3 +31,98 @@ colors:
 	assert.Assert(t, desc.Colors[1].G == 0)
 	assert.Assert(t, desc.Colors[1].B == 0)
 }
+
+func TestParseCheckerPattern(t *testing.T) {
+	yml := `
+patterns:
+  checker:
+    - name: gray_black_pattern
+      colorA: light_gray
+      colorB: black`
+
+	desc := ParseYaml(yml)
+
+	assert.Assert(t, desc != nil)
+	assert.Assert(t, len(desc.Patterns.Checker) == 1)
+	assert.Assert(t, len(desc.Colors) == 0)
+	assert.Assert(t, desc.Patterns.Checker[0].Name == "gray_black_pattern")
+	assert.Assert(t, desc.Patterns.Checker[0].ColorA == "light_gray")
+	assert.Assert(t, desc.Patterns.Checker[0].ColorB == "black")
+}
+
+func TestParseCheckerPatternWithTransforms(t *testing.T) {
+	yml := `
+patterns:
+  checker:
+    - name: gray_black_pattern
+      colorA: light_gray
+      colorB: black
+      transforms:
+        - type: rotation
+          x: 90.0
+        - type: translation
+          x: 100
+          y: 23.4`
+
+	desc := ParseYaml(yml)
+
+	assert.Assert(t, desc != nil)
+	assert.Assert(t, len(desc.Patterns.Checker) == 1)
+	assert.Assert(t, len(desc.Colors) == 0)
+	assert.Assert(t, len(desc.Patterns.Checker[0].Transforms) == 2)
+	assert.Assert(t, desc.Patterns.Checker[0].Name == "gray_black_pattern")
+	assert.Assert(t, desc.Patterns.Checker[0].ColorA == "light_gray")
+	assert.Assert(t, desc.Patterns.Checker[0].ColorB == "black")
+	assert.Assert(t, desc.Patterns.Checker[0].Transforms[0].Type == "rotation")
+	assert.Assert(t, desc.Patterns.Checker[0].Transforms[1].X == 100.0)
+}
+
+func TestParseMaterial(t *testing.T) {
+	yml := `
+materials:
+  - name: black_mat
+    color: black
+    ambient: 0.1
+    diffuse: 0.2
+    specular: 0.3
+    shininess: 0.4
+    reflective: 0.5
+    transparency: 0.6
+    refractiveIndex: 0.7`
+
+	desc := ParseYaml(yml)
+
+	assert.Assert(t, desc != nil)
+	assert.Assert(t, len(desc.Patterns.Checker) == 0)
+	assert.Assert(t, len(desc.Colors) == 0)
+	assert.Assert(t, len(desc.Materials) == 1)
+	assert.Assert(t, desc.Materials[0].Name == "black_mat")
+	assert.Assert(t, desc.Materials[0].Pattern == "")
+	assert.Assert(t, desc.Materials[0].Ambient == 0.1)
+	assert.Assert(t, desc.Materials[0].Diffuse == 0.2)
+	assert.Assert(t, desc.Materials[0].Specular == 0.3)
+	assert.Assert(t, desc.Materials[0].Shininess == 0.4)
+	assert.Assert(t, desc.Materials[0].Reflective == 0.5)
+	assert.Assert(t, desc.Materials[0].Transparency == 0.6)
+	assert.Assert(t, desc.Materials[0].RefractiveIndex == 0.7)
+}
+
+func TestParseSceneObject(t *testing.T) {
+	yml := `
+scene:
+  planes:
+    - name: p1
+      material: black_mat
+      transform: rotate_right_90`
+
+	desc := ParseYaml(yml)
+
+	assert.Assert(t, desc != nil)
+	assert.Assert(t, len(desc.Patterns.Checker) == 0)
+	assert.Assert(t, len(desc.Colors) == 0)
+	assert.Assert(t, len(desc.Materials) == 0)
+	assert.Assert(t, len(desc.Scene.Planes) == 1)
+	assert.Assert(t, desc.Scene.Planes[0].Name == "p1")
+	assert.Assert(t, desc.Scene.Planes[0].Material == "black_mat")
+	assert.Assert(t, desc.Scene.Planes[0].Transform == "rotate_right_90")
+}
